d7f090af1a499815e0cb2e12858cfc8f60e304f9
Shtantzer <93252552+Shtantzer@users.noreply.github.com>
2023-08-26 13:47:07 +0200

    Add files via upload

---
+++ haker.js
@@ -0,0 +1,19 @@
+
+
+const nadiMojuLokaciju = () => {
+    const status = document.querySelector('.status');
+
+    const success = (position) => {
+        console.log(position)
+    }
+
+    const error = () => {
+        status.textContent = "Unable to retrieve your location";
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error);
+
+   
+}
+
+document.querySelector('.find-state').addEventListener('click', nadiMojuLokaciju);
